f641519f6b194e5abf28cb3fd65215d20c2feae6
Tridib <tridib.ewu.bd@gmail.com>
2024-11-20 00:52:17 +0600

    Fourth commit of the Donation App Frontend Project

---
+++ js/donateMoney.js
@@ -0,0 +1,17 @@
+/** @format */
+
+document
+  .getElementById("btnMoney-Noakhali")
+  .addEventListener("click", function () {
+    console.log("its working");
+  });
+
+document.getElementById("btnMoney-Feni").addEventListener("click", function () {
+  console.log("its working");
+});
+
+document
+  .getElementById("btnMoney-Quota")
+  .addEventListener("click", function () {
+    console.log("its working");
+  });

8ff57d9c3b676e4df182369a982c257d4f75da2f
Tridib Chakraborty <tridib.ewu.bd@gmail.com>
2025-04-19 20:33:04 +0600

    New commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @format */
+
+module.exports = {
+  //...
+  daisyui: {
+    themes: [
+      "light",
+      "dark",
+      "cupcake",
+      {
+        light: {
+          ...require("daisyui/src/theming/themes")["light"],
+          primary: "blue",
+          secondary: "teal",
+        },
+      },
+    ],
+  },
+};
+<html data-theme='cupcake'></html>;

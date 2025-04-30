309d04e3ff41cfe5c216b3a85043e29c27d452ac
Tridib Chakraborty <tridib.ewu.bd@gmail.com>
2025-05-01 01:55:35 +0600

    Add some comments and understanding

--- js/donateMoney.js
+++ js/donateMoney.js
@@ -2,8 +2,20 @@
 
 document
   .getElementById("btnMoney-Noakhali")
-  .addEventListener("click", function () {
-    console.log("its working");
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const donateMoney = getInputFieldValueById("donateNoakhali");
+
+    if (isNaN(donateMoney)) {
+      alert("Failed to cash out");
+      return;
+    }
+
+    const donationOfFeni = getTextFieldValueById("donateMoneyNoakhaliFlood");
+
+    const newFeniDonation = donationOfFeni + donateMoney;
+    
+    document.getElementById("donateMoneyNoakhaliFlood").innerText = newFeniDonation;
   });
 
 document.getElementById("btnMoney-Feni").addEventListener("click", function () {
